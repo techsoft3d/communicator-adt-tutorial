1df7b60dd6ceeb07618ff84e762a19cb433d625e
Jingsong Tan <jingsong.tan@techsoft3d.com>
2021-08-24 21:32:31 -0500

    seperate config file

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const ADT_URL = 'https://xxx.azure.net/digitaltwins/';
+var adtConfig = require(__dirname + "/adt.config.js");
+const ADT_URL = 'https://' + adtConfig.hostname + '/';
 
 var express = require("express");
 const request = require('request');
@@ -9,90 +10,132 @@ const server = require('http').createServer(app);
 const { access } = require("fs");
 
 app.listen(3000, () => {
-    console.log("Server running on port 3000"); 
+  console.log("Server running on port 3000");
 });
 
 app.use(express.static('src'));
+app.use(express.json());
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/src/index.html');
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/src/index.html');
 });
 
-app.get("/data/*", (req, res, next) => {  
-    const headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + req.headers.authorization
-    }
-    new Promise((resolve, reject) => {
-        request.get({
-            url: ADT_URL + req.params[0] + '?api-version=2020-10-31',
-            headers: headers
-        }, function (error, response, body) {
-            if (response != undefined && response.statusCode != undefined && response.statusCode !== 201) {
-                resolve(body);
-            } 
-        });
-    }).then( (result) => {
-        res.send(result);
+app.get('/adtConfig', function(req, res) {
+  res.send(adtConfig);
+});
+
+// req.params[0] is the adtId in onkects.json
+app.get("/data/*", (req, res, next) => {
+  console.log("data " + req.params[0]);
+  const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + req.headers.authorization
+  }
+
+  const url = ADT_URL + 'digitaltwins/' + req.params[0] + '?api-version=2020-10-31';
+
+  new Promise((resolve, reject) => {
+    request.get({
+      url: url,
+      headers: headers
+    }, function (error, response, body) {
+      if (error) throw new Error(error);
+      if (response != undefined && response.statusCode != undefined && response.statusCode !== 201) {
+        resolve(body);
+      }
     });
+  }).then((result) => {
+    res.send(result);
+  });
 });
 
 app.get("/relationships/*", (req, res, next) => {
-    const headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + req.headers.authorization
-    }
-    new Promise((resolve, reject) => {
-        request.get({
-            url: ADT_URL + req.params[0] + '/relationships' + '?api-version=2020-10-31',
-            headers: headers
-        }, function (error, response, body) {
-            if (response != undefined && response.statusCode != undefined && response.statusCode !== 201) {
-                resolve(body);
-            }
-        });
-    }).then( (result) => {
-        res.send(result);
+  const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + req.headers.authorization
+  }
+  new Promise((resolve, reject) => {
+    request.get({
+      url: ADT_URL + req.params[0] + '/relationships' + '?api-version=2020-10-31',
+      headers: headers
+    }, function (error, response, body) {
+      if (response != undefined && response.statusCode != undefined && response.statusCode !== 201) {
+        resolve(body);
+      }
     });
+  }).then((result) => {
+    res.send(result);
+  });
 });
 
 app.post("/force_alert", (req, res, next) => {
-    var options = {
-        'method': 'PATCH',
-        'url': ADT_URL + 'Grinding.pu01.l01' + '?api-version=2020-10-31',
-        'headers': {
-          'Content-Type': 'application/json-patch+json',
-          'Authorization': 'Bearer ' + req.headers.authorization,
-        },
-        body: "[{\n\"op\": \"replace\",\n\"path\": \"/vibrationAlert\",\n\"value\": true\n},\n{\n\"op\": \"replace\",\n\"path\": \"/Vibration\",\n\"value\": 300\n}\n]"
-    };
-    new Promise((resolve, reject) => {
-        request(options, (error, response) => {
-            if (error) throw new Error(error);
-            console.log(response.body);
-        });
-    }).then( (result) => {
-        res.send(result);
+  var options = {
+    'method': 'PATCH',
+    'url': ADT_URL + 'Grinding.pu01.l01' + '?api-version=2020-10-31',
+    'headers': {
+      'Content-Type': 'application/json-patch+json',
+      'Authorization': 'Bearer ' + req.headers.authorization,
+    },
+    body: "[{\n\"op\": \"replace\",\n\"path\": \"/vibrationAlert\",\n\"value\": true\n},\n{\n\"op\": \"replace\",\n\"path\": \"/Vibration\",\n\"value\": 300\n}\n]"
+  };
+  new Promise((resolve, reject) => {
+    request(options, (error, response) => {
+      if (error) throw new Error(error);
     });
+  }).then((result) => {
+    res.send(result);
+  });
 });
 
 app.post("/reset_alert", (req, res, next) => {
 
-    var options = {
-        'method': 'PATCH',
-        'url': ADT_URL + 'Grinding.pu01.l01' + '?api-version=2020-10-31',
-        'headers': {
-          'Content-Type': 'application/json-patch+json',
-          'Authorization': 'Bearer ' + req.headers.authorization,
-        },
-        body: "[{\n\"op\": \"replace\",\n\"path\": \"/vibrationAlert\",\n\"value\": false\n},\n{\n\"op\": \"replace\",\n\"path\": \"/Vibration\",\n\"value\": 260\n}\n]"
-    };
-    new Promise((resolve, reject) => {
-        request(options, (error, response) => {
-            if (error) throw new Error(error);
-            console.log(response.body);
-        });
-    }).then( (result) => {
-        res.send(result);
+  var options = {
+    'method': 'PATCH',
+    'url': ADT_URL + 'Grinding.pu01.l01' + '?api-version=2020-10-31',
+    'headers': {
+      'Content-Type': 'application/json-patch+json',
+      'Authorization': 'Bearer ' + req.headers.authorization,
+    },
+    body: "[{\n\"op\": \"replace\",\n\"path\": \"/vibrationAlert\",\n\"value\": false\n},\n{\n\"op\": \"replace\",\n\"path\": \"/Vibration\",\n\"value\": 260\n}\n]"
+  };
+  new Promise((resolve, reject) => {
+    request(options, (error, response) => {
+      if (error) throw new Error(error);
+      console.log(response.body);
     });
+  }).then((result) => {
+    res.send(result);
+  });
 });
+
+/// For updating data
+/// Updading Fanning, Grinding, and Molding
+app.post("/update_data", (req, res, next) => {
+  var options = {
+    'method': 'PATCH',
+    'url': ADT_URL + 'digitaltwins/' + capitalizeFirstLetter(req.body["device"]) + '.pu01.l01' + '?api-version=2020-10-31',
+    'headers': {
+      'Content-Type': 'application/json-patch+json',
+      'Authorization': 'Bearer ' + req.headers.authorization,
+    },
+    body: req.body["content"]
+  };
+
+  request(options, (error, response) => {
+
+  });
+  res.send("post request");
+
+  // new Promise((resolve, reject) => {
+  //   request(options, (error, response) => {
+  //     if (error) throw new Error(error);
+  //     if (response.statusCode == 202) resolve(response);
+  //   });
+  // }).then((result) => {
+  //   res.send(result);
+  // });
+});
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
--- src/js/adt_helper.js
+++ src/js/adt_helper.js
@@ -1,116 +1,219 @@
-const CLIENT_ID = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
-const AUTHORITY = "https://login.microsoftonline.com/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
+// App ID
+var CLIENT_ID = null;
+// https://login.microsoftonline.com/<tenantID>
+var AUTHORITY = null;
 const REDIRECT_URL = "http://localhost:3000";
 
+function setConfig(data) {
+  CLIENT_ID = data.appId;
+  AUTHORITY = "https://login.microsoftonline.com/" + data.tenant;
+}
+
+
+// Access token will be granted once you loged in.
 accessToken = null;
 
 const invokeWithLoading = async callback => {
-    let response = null;
-    try {
-        response = await callback();
-    } catch (e) {
-        throw e;
-    }
+  let response = null;
+  try {
+    response = await callback();
+  } catch (e) {
+    throw e;
+  }
 
-    return response;
+  return response;
 };
 
 async function azureIoTLogin() {
-    // Config object to be passed to Msal on creation
-    const msalConfig = {
-        auth: {
-            clientId: CLIENT_ID,
-            authority: AUTHORITY,
-            redirectUri: REDIRECT_URL,
-        },
-        cache: {
-            cacheLocation: "localStorage",
-            storeAuthStateInCookie: true,
-        }
-    };
+  console.log(CLIENT_ID);
 
-    const myMSALObj = new Msal.UserAgentApplication(msalConfig);
-
-    const loginRequest = { scopes: ["https://digitaltwins.azure.net/.default"] };
-
-    if (!myMSALObj.getAccount()) {
-        await invokeWithLoading(async () => await myMSALObj.loginPopup(loginRequest));
+  // Config object to be passed to Msal on creation
+  const msalConfig = {
+    auth: {
+      clientId: CLIENT_ID,
+      authority: AUTHORITY,
+      redirectUri: REDIRECT_URL,
+    },
+    cache: {
+      cacheLocation: "localStorage",
+      storeAuthStateInCookie: true,
     }
+  };
 
-    // If the user is already logged in you can acquire a token
-    if (myMSALObj.getAccount()) {
-        try {
-            const response = await myMSALObj.acquireTokenSilent(loginRequest);
-            accessToken = response.accessToken;
-        } catch (err) {
-            // Could also check if err instance of InteractionRequiredAuthError if you can import the class
-            if (err.name === "InteractionRequiredAuthError") {
-                const response = await invokeWithLoading(async () => await myMSALObj.acquireTokenPopup(loginRequest));
-                accessToken = response.accessToken;
-            }
-        }
+  const myMSALObj = new Msal.UserAgentApplication(msalConfig);
+
+  const loginRequest = { scopes: ["https://digitaltwins.azure.net/.default"] };
+
+  console.log(myMSALObj.getAccount());
+
+  if (!myMSALObj.getAccount()) {
+    await invokeWithLoading(async () => await myMSALObj.loginPopup(loginRequest));
+  }
+
+  // If the user is already logged in you can acquire a token
+  if (myMSALObj.getAccount()) {
+    try {
+      const response = await myMSALObj.acquireTokenSilent(loginRequest);
+      accessToken = response.accessToken;
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+      // Could also check if err instance of InteractionRequiredAuthError if you can import the class
+      if (err.name === "InteractionRequiredAuthError") {
+        const response = await invokeWithLoading(async () => await myMSALObj.acquireTokenPopup(loginRequest));
+        accessToken = response.accessToken;
+      }
     }
+  }
 }
+// param "object" is object.adtID
+function get_data(object) {
+  return new Promise((resolve, reject) => {
+    if (!accessToken) {
+      console.log("No access token!");
+      return;
+    }
 
-function get_data(object){
-    return new Promise((resolve, reject) => {
-        if(!accessToken) return;
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
 
-        var xhr = new XMLHttpRequest();
-        xhr.withCredentials = true;
+    xhr.addEventListener("readystatechange", function () {
+      console.log("ready state change " + this.readyState);
+      if (this.readyState === 4) {
+        resolve(this.responseText);
+      }
+    });
 
-        xhr.addEventListener("readystatechange", function() {
-            if(this.readyState === 4) {
-                resolve(this.responseText);
-            }
-        });
+    xhr.addEventListener("error", function (e) {
+      console.log("error!");
+      console.log(e);
+    });
 
-        xhr.open("GET", "http://localhost:3000/data/" + object);        
+    xhr.open("GET", "http://localhost:3000/data/" + object);
 
-        xhr.setRequestHeader('Authorization', accessToken);
-        
-        xhr.send();        
-    })
+    xhr.setRequestHeader('Authorization', accessToken);
+
+    xhr.send();
+    console.log(xhr);
+  })
 }
 
 function force_alert() {
-    return new Promise((resolve, reject) => {
-        if (!accessToken) reject();
+  return new Promise((resolve, reject) => {
+    if (!accessToken) reject();
 
-        var xhr = new XMLHttpRequest();
-        xhr.withCredentials = true;
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
 
-        xhr.addEventListener("readystatechange", function() {
-            if(this.readyState === 4) {
-                console.log(this.responseText);
-                resolve(this.responseText);
-            }
-        });
-
-        xhr.open("POST", 'http://localhost:3000/force_alert');        
-        xhr.setRequestHeader('Authorization', accessToken);
-        xhr.setRequestHeader("Content-Type", "application/json")
-        xhr.send();        
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+        console.log(this.responseText);
+        resolve(this.responseText);
+      }
     });
+
+    xhr.open("POST", 'http://localhost:3000/force_alert');
+    xhr.setRequestHeader('Authorization', accessToken);
+    xhr.setRequestHeader("Content-Type", "application/json")
+    xhr.send();
+  });
 }
 
 function reset_alert() {
-    return new Promise((resolve, reject) => {
-        if (!accessToken) reject();
+  return new Promise((resolve, reject) => {
+    if (!accessToken) reject();
 
-        var xhr = new XMLHttpRequest();
-        xhr.withCredentials = true;
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
 
-        xhr.addEventListener("readystatechange", function() {
-            if(this.readyState === 4) {
-                console.log(this.responseText);
-                resolve(this.responseText);
-            }
-        });
-
-        xhr.open("POST", 'http://localhost:3000/reset_alert');        
-        xhr.setRequestHeader('Authorization', accessToken);
-        xhr.setRequestHeader("Content-Type", "application/json")
-        xhr.send();        
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+        console.log(this.responseText);
+        resolve(this.responseText);
+      }
     });
+
+    xhr.open("POST", 'http://localhost:3000/reset_alert');
+    xhr.setRequestHeader('Authorization', accessToken);
+    xhr.setRequestHeader("Content-Type", "application/json")
+    xhr.send();
+  });
+}
+
+/// For updating ADT data
+
+function update_data() {
+  console.log("update");
+  if (!accessToken) return;
+
+  update_step_data("fanning");
+  update_step_data("grinding");
+  update_step_data("molding");
+}
+
+// deviceType: "grinding", "fanning", or "molding"
+function update_step_data(deviceType) {
+  if (!accessToken) return;
+
+  var xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
+
+  xhr.addEventListener("error", function (e) {
+    console.log(e);
+  });
+
+  xhr.open("POST", 'http://localhost:3000/update_data');
+  xhr.setRequestHeader('Authorization', accessToken);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.send(JSON.stringify({
+    device: deviceType,
+    content: generateMessage(deviceType)
+  }));
+}
+
+// Generating message for "grinding", "fanning", or "molding"
+// Return a string
+function generateMessage(deviceType) {
+  const fanSpeed = 10 + (Math.random() * 4); // range: [10, 14]
+  const temperature = 200 + (Math.random() * 10); // range: [200, 300]
+  const powerUsage = 60 + (Math.random() * 20); // range: [60, 80]
+  const force = 300 + (Math.random() * 10); // range: [300, 400]
+  const vibration = 80 + Math.floor(Math.random() * Math.floor(80)); // range: [99, 199]
+  const roastingTime = 30 + (Math.floor(Math.random() * 100)); //range [30, 50]
+
+  var data = {};
+  switch (deviceType) {
+    case "grinding":
+      data = {
+        ChasisTemperature: temperature,
+        PowerUsage: powerUsage,
+        GrindingTime: roastingTime,
+        Force: force
+      };
+      break;
+    case "fanning":
+      data = {
+        ChasisTemperature: temperature,
+        PowerUsage: powerUsage,
+        FanSpeed: fanSpeed,
+        RoastingTime: roastingTime
+      };
+      break;
+    case "molding":
+      data = {
+        ChasisTemperature: temperature,
+        PowerUsage: powerUsage
+      };
+  }
+
+  var mapped_data = [];
+  for (const [key, value] of Object.entries(data)) {
+    mapped_data.push({
+      op: "replace",
+      path: "/" + key,
+      value: value
+    });
+  }
+
+  return JSON.stringify(mapped_data);
 }
